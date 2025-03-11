92279831c12362b15ddb7be80eac4ab68dac63f2
Kiran Dhiyad <96478470+KiranDhiyad@users.noreply.github.com>
2025-03-11 10:50:45 +0530

    Update RetrofitHelper.kt
    Remove dummy var

--- app/src/main/java/com/karan/pratical/RetrofitHelper.kt
+++ app/src/main/java/com/karan/pratical/RetrofitHelper.kt
@@ -4,7 +4,6 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitHelper {
-    //    public static final String SERVER_URL ="http://143.244.142.185/api/";
     val SERVER_URL = "https://itunes.apple.com/"
 
     fun getInstance() : Retrofit{
